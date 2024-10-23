c6530eea4c7af59eb4e2b8565900d05db21bb55d
Yukash <sivarajyukash@gmail.com>
2024-10-23 22:15:25 +1100

    downgraded react to v18

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import {NextUIProvider} from "@nextui-org/react";
+import {Providers} from "./providers";
 
 import "./globals.css";
 
@@ -30,9 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NextUIProvider>
+        <Providers>
           {children}
-        </NextUIProvider>
+        </Providers>
       </body>
     </html>
   );
