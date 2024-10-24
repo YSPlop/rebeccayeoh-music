4129b1b1540537815e53c6aba049792d1808a66f
YSPlop <sivarajyukash@gmail.com>
2024-10-24 18:43:34 +1100

    trying to fix prod error for recaptcha

--- app/api/recaptchaSubmit/route.ts
+++ app/api/recaptchaSubmit/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import "dotenv/config"
 import axios from "axios";
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(request: Request, response: Response) {
