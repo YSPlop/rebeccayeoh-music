907d80f4af0943de49eff635d30f842e24e88353
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2024-10-24 17:46:41 +1100

    update images

--- components/MainPageContent.tsx
+++ components/MainPageContent.tsx
@@ -5,7 +5,7 @@ const MainPageContent: React.FC = () => {
     const introMessage = "Are you interested in taking up piano lessons, but you've never had time before? Or getting your children into a music program for extra curricular activities? We take beginners of all ages!";
 
     return (
-        <div className="mx-auto w-[100%] md:w-[80%] bg-pink-100 h-full items-center pt-32">
+        <div className="mx-auto w-[100%] md:w-[80%] bg-pink-100 h-screen items-center pt-20">
             <h1 className="text-2xl font-bold text-gray-800 text-center px-10 mb-8">
                 {introMessage}
             </h1>
@@ -18,22 +18,33 @@ const MainPageContent: React.FC = () => {
                 Lessons are based in Lane Cove, the lower north shore of Sydney. Non school aged students &
                 adults have the flexibility of having lessons in the comfort of their own home.
             </p>
+            <p className="text-lg text-gray-600 mb-4 text-center">
+                <span className="font-bold">Students will learn:</span>
+            </p>
             <div className="flex flex-col md:flex-row justify-center space-x-8 mb-16">
                 <div className="flex flex-col items-center">
-                    <img src="/path/to/image1.jpg" alt="Description 1" className="w-32 h-32 object-cover mb-2" />
-                    <span className="text-center text-gray-600">Caption for Image 1</span>
+                    <img src="/images/student-teacher.jpg" alt="Description 1" className="w-64 h-64 object-cover mb-2" />
+                    <span className="text-center text-gray-600">
+                        How to read and play music <br /> (classical, contemporary and fun songs)
+                    </span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <img src="/path/to/image2.jpg" alt="Description 2" className="w-32 h-32 object-cover mb-2" />
-                    <span className="text-center text-gray-600">Caption for Image 2</span>
+                    <img src="/images/theory.jpg" alt="Description 1" className="w-64 h-64 object-cover mb-2" />
+                    <span className="text-center text-gray-600">
+                        General knowledge <br /> (about music, including composers)
+                    </span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <img src="/path/to/image3.jpg" alt="Description 3" className="w-32 h-32 object-cover mb-2" />
-                    <span className="text-center text-gray-600">Caption for Image 3</span>
+                    <img src="/images/student-teacher.jpg" alt="Description 1" className="w-64 h-64 object-cover mb-2" />
+                    <span className="text-center text-gray-600">
+                        Aural skills <br /> (pitch - listening and singing and rhythm)
+                    </span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <img src="/path/to/image4.jpg" alt="Description 4" className="w-32 h-32 object-cover mb-2" />
-                    <span className="text-center text-gray-600">Caption for Image 4</span>
+                    <img src="/images/student-teacher.jpg" alt="Description 1" className="w-64 h-64 object-cover mb-2" />
+                    <span className="text-center text-gray-600">
+                        Theory and fundamentals of music
+                    </span>
                 </div>
             </div>
         </div>
